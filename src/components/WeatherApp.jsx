6d1e2e1efb60665052036f43603f07a2785f13bc
Daniel Albanez <danielalbanez40@gmail.com>
2022-09-07 09:36:03 -0300

    Solucionando errores en produccion

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -1,47 +1,36 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import WeatherForm from './WeatherForm';
 import WeatherMainInfo from './WeatherMainInfo';
-import styles from "./weatherApp.module.css";
+import styles from './weatherApp.module.css';
 
 const WeatherApp = () => {
+  const [weather, setWeather] = useState(null);
 
-    const [weather, setWeather] = useState(null);
+  console.log({ styles });
+  useEffect(() => {
+    loadInfo();
+  }, []);
+  useEffect(() => {
+    document.title = `Weather | ${weather?.location.name}`;
 
-    console.log({styles});
-    useEffect(() => {
-      loadInfo();
+    return () => {};
+  }, [weather]);
 
-    }, [])
-    useEffect(() => {
-      document.title = `Weather | ${weather?.location.name}`;
-    
-      return () => {
-        
-      }
-    }, [weather])
-    
-    
+  async function loadInfo(city = 'london') {
+    try {
+      const request = await fetch(
+        `${process.env.REACT_APP_URL}&key=${process.env.REACT_APP_KEY}&q=${city}`,
+      );
+      const json = await request.json();
+      setWeather(json);
+      console.log(json);
+    } catch (error) {}
+  }
 
-    async function loadInfo(city = 'london') {
-        
-        try {
-            
-            const request = await fetch(`${process.env.REACT_APP_URL}&key=${process.env.REACT_APP_KEY}&q=${city}`);
-            const json = await request.json();
-            setWeather(json)
-            console.log(json);
-
-        } catch (error) {
-            
-        }
-
-
-    }
-
-    function handleChangeCity(city) {
-        setWeather(null)
-        loadInfo(city);
-    }
+  function handleChangeCity(city) {
+    setWeather(null);
+    loadInfo(city);
+  }
 
   return (
     <div className={styles.weatherContainer}>
@@ -50,6 +39,6 @@ const WeatherApp = () => {
       <WeatherMainInfo weather={weather} />
     </div>
   );
-}
+};
 
 export default WeatherApp;
